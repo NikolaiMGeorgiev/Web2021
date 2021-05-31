34790ff29a0d5895d57ca6e1313949582d821afb
erikz99 <erikzdravkov99@gmail.com>
2021-05-31 20:06:04 +0300

    Added session request handler

--- src/SessionRequestHandler.php
+++ src/SessionRequestHandler.php
@@ -4,38 +4,32 @@
     
     AppBootStrap::init();
 
-    class UserRequestHandler {
-        public static function createUser($userInfo) {
+    class SessionRequestHandler {
+
+        public static function login(array $loginData) : bool {
+            
             $connection = self::initConnection();
 
-            $hashed_password = password_hash($userInfo["password"], PASSWORD_DEFAULT);
-
-            $stmt = $connection->prepare("INSERT INTO users (name,password,email,userTypeId) VALUES (:name, :password, :email, :userTypeId)");
-            $success = $stmt->execute([
-                "name" => $userInfo["name"],
-                "password" => $hashed_password,
-                "email" => $userInfo["email"],
-                "userTypeId" => $userInfo["userTypeId"]
+            $stmt = $connection->prepare("SELECT * FROM users WHERE name=:email");
+            $stmt->execute([
+                "name" => $loginData["email"]
             ]);
+            
+            $user = $stmt->fetch();
 
-            $userId = $connection->lastInsertId();
-    
-            $stmt = $connection->prepare("SELECT * FROM usertypes WHERE id=:userTypeId");
-            $success = $stmt->execute([
-                "userTypeId" => $userInfo["userTypeId"]
-            ]);
-
-            if ($stmt->fetch(PDO::FETCH_ASSOC)["code"] == "STUDENT") {
-                $stmt = $connection->prepare("INSERT INTO students_details (fn,year,degree,userId) VALUES (:fn,:year,:degree,:userId)");
-                $success = $stmt->execute([
-                    "fn" => $userInfo["fn"],
-                    "year" => $userInfo["fn"],
-                    "degree" => $userInfo["degree"],
-                    "userId" => $userId
-                ]);
+            if (!$user) {
+                return false;
+            } else if (!password_verify($loginData["password"], $user["password"])) {
+                return false;
             }
 
-            return $userId;
+            return true;
+        }
+
+        public static function requireLoggedUser() {
+            if (!$_SESSION["logged"]) {
+                throw new AuthorizationException();
+            }
         }
 
         private static function initConnection() {
