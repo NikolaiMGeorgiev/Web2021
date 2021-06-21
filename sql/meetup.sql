879e22901d237e8fd571686e645a140e6d7de575
NikolaiMGeorgiev <nikolaimgeorgiev@abv.bg>
2021-06-21 21:40:51 +0300

    sql final

--- sql/meetup.sql
+++ sql/meetup.sql
@@ -49,14 +49,6 @@ CREATE TABLE `queues` (
   `active` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `queues`
---
-
-INSERT INTO `queues` (`id`, `userIndex`, `userId`, `roomId`, `active`) VALUES
-(26, 3, 17, 55, 0),
-(27, 2, 20, 55, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -75,13 +67,6 @@ CREATE TABLE `rooms` (
   `activated` int(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `rooms`
---
-
-INSERT INTO `rooms` (`id`, `name`, `waitingInterval`, `meetInterval`, `start`, `userId`, `currentTime`, `state`) VALUES
-(55, 'Web', 2, 15, '2021-06-17 17:32:21', 8, '2021-06-17 17:32:21', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -94,17 +79,6 @@ CREATE TABLE `schedule` (
   `roomId` int(10) NOT NULL,
   `place` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `schedule`
---
-
-INSERT INTO `schedule` (`id`, `userId`, `roomId`, `place`) VALUES
-(13, 15, 55, 1),
-(14, 20, 55, 2),
-(15, 17, 55, 3),
-(16, 18, 55, 4),
-(17, 19, 55, 5);
 
 -- --------------------------------------------------------
 
@@ -120,17 +94,6 @@ CREATE TABLE `students_details` (
   `userId` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `students_details`
---
-
-INSERT INTO `students_details` (`id`, `fn`, `degree`, `year`, `userId`) VALUES
-(4, 351515, 'swebw', '3', 15),
-(5, 63333, 'СИ', '6', 17),
-(6, 64444, 'СИ', '6', 18),
-(7, 61111, 'СИ', '6', 19),
-(8, 65555, 'СИ', '6', 20);
-
 -- --------------------------------------------------------
 
 --
@@ -145,18 +108,6 @@ CREATE TABLE `users` (
   `userTypeId` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `password`, `email`, `userTypeId`) VALUES
-(8, 'Преподвател Преподавателски', '$2y$10$30ocrCeVl4Jm4NA0SLojCOqmx/9ASo29Ich124vQg75lrLdro2M26', 'test@example.com', 2),
-(15, 'Първи Първов', '$2y$10$hKnv/KoEu.aWVxk7xuyNbOtKaEg0/6DT1o.SFp/30uSUjKicNaQ8.', 'test2@example.com', 1),
-(17, 'Николай Георгиев', '$2y$10$LKY5lw8tJLCvazkJCiy//uT9N1aiqcytXniMY0let2Es3XFIhyG6O', 'ng@example.com', 1),
-(18, 'Ерик Здравков', '$2y$10$KZHODb2bz4ct6hn23q2YGOAZWeNwlZ61l6QbYZi4KXeGOo39JyAua', 'ez@example.com', 1),
-(19, 'Никол Георгиева', '$2y$10$Y3AEagZRpMTc3HOH1O/l1.bwUuks1pc9u04xmWkLA8qz5nRaUbiwq', 'ng2@example.com', 1),
-(20, 'Ерика Здравкова', '$2y$10$W3kfjWvz7T27JO4kMydrPeGcWtDnTKXF5nx36jomc.XZTRhxy7JJe', 'ez2@example.com', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -168,14 +119,6 @@ CREATE TABLE `usertypes` (
   `name` varchar(200) NOT NULL,
   `code` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `usertypes`
---
-
-INSERT INTO `usertypes` (`id`, `name`, `code`) VALUES
-(1, 'Студент', 'STUDENT'),
-(2, 'Преподавател', 'TEACHER');
 
 --
 -- Indexes for dumped tables
